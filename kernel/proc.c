a95dec46d4443c579f47e793e7d731cce2d34388
Ben Holmes <bcwh@csail.mit.edu>
2025-11-11 20:23:14 -0500

    fs lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -98,7 +98,6 @@ allocpid()
   pid = nextpid;
   nextpid = nextpid + 1;
   release(&pid_lock);
-
   return pid;
 }
 
@@ -131,6 +130,10 @@ found:
     release(&p->lock);
     return 0;
   }
+
+#ifdef LAB_LOCK
+  p->pincpu = 0;
+#endif
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -202,6 +205,7 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+
   return pagetable;
 }
 
@@ -264,7 +268,7 @@ kfork(void)
   if((np = allocproc()) == 0){
     return -1;
   }
-
+  
   // Copy user memory from parent to child.
   if(uvmcopy(p->pagetable, np->pagetable, p->sz) < 0){
     freeproc(np);
@@ -272,6 +276,7 @@ kfork(void)
     return -1;
   }
   np->sz = p->sz;
+
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
@@ -290,13 +295,14 @@ kfork(void)
   pid = np->pid;
 
   release(&np->lock);
-
+  
   acquire(&wait_lock);
   np->parent = p;
   release(&wait_lock);
 
   acquire(&np->lock);
   np->state = RUNNABLE;
+
   release(&np->lock);
 
   return pid;
@@ -337,6 +343,7 @@ kexit(int status)
     }
   }
 
+  
   begin_op();
   iput(p->cwd);
   end_op();
@@ -434,27 +441,39 @@ scheduler(void)
     intr_on();
     intr_off();
 
-    int found = 0;
+    int nproc = 0;
     for(p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
+      if(p->state != UNUSED) {
+        nproc++;
+      }
+#ifdef LAB_LOCK
+      if(p->pincpu && p->pincpu != c) {
+        release(&p->lock);
+        continue;
+      }
+#endif
       if(p->state == RUNNABLE) {
         // Switch to chosen process.  It is the process's job
         // to release its lock and then reacquire it
         // before jumping back to us.
         p->state = RUNNING;
         c->proc = p;
+        
         swtch(&c->context, &p->context);
 
         // Process is done running for now.
         // It should have changed its p->state before coming back.
         c->proc = 0;
-        found = 1;
       }
       release(&p->lock);
     }
-    if(found == 0) {
+    if(nproc <= 2) {   // only init and sh exist
       // nothing to run; stop running on this core until an interrupt.
+      intr_on();
+#ifndef LAB_FS
       asm volatile("wfi");
+#endif
     }
   }
 }
@@ -685,3 +704,5 @@ procdump(void)
     printf("\n");
   }
 }
+
+
